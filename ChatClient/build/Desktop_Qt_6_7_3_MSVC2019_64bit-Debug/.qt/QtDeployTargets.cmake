b0d79800d5b449ebfa77e0208e11a69f328a27c8
Shilin Liang <shilinnull@163.com>
2025-08-29 13:12:37 +0800

    Remove build directory from version control and update .gitignore

--- ChatClient/build/Desktop_Qt_6_7_3_MSVC2019_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ChatClient_FILE F:/ChatsSystem/ChatClient/build/Desktop_Qt_6_7_3_MSVC2019_64bit-Debug/ChatClient.exe)
-set(__QT_DEPLOY_TARGET_ChatClient_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ChatClient_RUNTIME_DLLS C:/Qt/6.7.3/msvc2019_64/bin/Qt6Widgetsd.dll;C:/Qt/6.7.3/msvc2019_64/bin/Qt6Guid.dll;C:/Qt/6.7.3/msvc2019_64/bin/Qt6Cored.dll)
--- ChatClient/build/Desktop_Qt_6_7_3_MSVC2019_64bit-Debug/.qt/deploy_ChatClient_675a5e9132.cmake
+++
@@ -1,8 +0,0 @@
-include(F:/ChatsSystem/ChatClient/build/Desktop_Qt_6_7_3_MSVC2019_64bit-Debug/.qt/QtDeploySupport.cmake)
-include("${CMAKE_CURRENT_LIST_DIR}/ChatClient-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_ALL_MODULES_FOUND_VIA_FIND_PACKAGE "ZlibPrivate;EntryPointPrivate;Core;Gui;Widgets")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE F:/ChatsSystem/ChatClient/build/Desktop_Qt_6_7_3_MSVC2019_64bit-Debug/ChatClient.exe
-    GENERATE_QT_CONF
-)
